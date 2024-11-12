cd33becf051d50360edb5687ab8582dc74c22ce6
svedev0 <emanuel@kulich.me>
2024-11-12 22:33:01 +0100

    First code commit

---
+++ FFMpegCoreExtensions.cs
@@ -0,0 +1,66 @@
+﻿using FFAO = FFMpegCore.FFMpegArgumentOptions;
+
+namespace RtspHlsRelay;
+
+public static class FFMpegCoreExtensions
+{
+	public static FFAO WithNoBanner(this FFAO opts)
+	{
+		return opts.WithCustomArgument("-hide_banner");
+	}
+
+	public static FFAO WithReconnect(this FFAO opts)
+	{
+		return opts.WithCustomArgument("-reconnect 1 -reconnect_streamed 1");
+	}
+
+	public static FFAO WithReconnectDelay(this FFAO opts, int seconds)
+	{
+		return opts.WithCustomArgument($"-reconnect_delay_max {seconds}");
+	}
+
+	public static FFAO WithHlsTime(this FFAO opts, int seconds)
+	{
+		return opts.WithCustomArgument($"-hls_time {seconds}");
+	}
+
+	public static FFAO WithHlsPlaylistSize(this FFAO opts, int size)
+	{
+		return opts.WithCustomArgument($"-hls_list_size {size}");
+	}
+
+	public static FFAO WithHlsFlags(this FFAO opts, IEnumerable<string> flags)
+	{
+		return opts.WithCustomArgument($"-hls_flags {string.Join("+", flags)}");
+	}
+
+	public static FFAO WithHlsSegmentFilename(this FFAO opts, string filename)
+	{
+		return opts.WithCustomArgument($"-hls_segment_filename \"{filename}\"");
+	}
+
+	public static FFAO WithPersistentHttp(this FFAO opts)
+	{
+		return opts.WithCustomArgument("-http_persistent 1");
+	}
+
+	public static FFAO WithKeyframeInterval(this FFAO opts, int interval)
+	{
+		return opts.WithCustomArgument($"-g {interval}");
+	}
+
+	public static FFAO WithFlags(this FFAO opts, IEnumerable<string> flags)
+	{
+		return opts.WithCustomArgument($"-flags +{string.Join("+", flags)}");
+	}
+
+	public static FFAO WithFFlags(this FFAO opts, IEnumerable<string> flags)
+	{
+		return opts.WithCustomArgument($"-fflags +{string.Join("+", flags)}");
+	}
+
+	public static FFAO StartAtZero(this FFAO opts)
+	{
+		return opts.WithCustomArgument("-start_at_zero");
+	}
+}
---
+++ Program.cs
@@ -0,0 +1,104 @@
+﻿using DotNetEnv;
+using FFMpegCore;
+using FFMpegCore.Enums;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace RtspHlsRelay;
+
+/*
+http-server 'C:\path\to\stream' -p 8080
+dotnet run
+*/
+
+public class Program
+{
+	public static void Main(string[] args)
+	{
+		Env.Load(".env");
+
+		var builder = Host.CreateDefaultBuilder(args);
+		builder.ConfigureServices((_, s) => s.AddHostedService<FFMpegService>());
+
+		var app = builder.Build();
+		app.Run();
+	}
+}
+
+public class FFMpegService(IHostApplicationLifetime lifetime) : IHostedService
+{
+	private Action? cancelAction;
+
+	public Task StartAsync(CancellationToken cancellationToken)
+	{
+		lifetime.ApplicationStarted.Register(() =>
+		{
+			Task.Run(async () =>
+			{
+				try
+				{
+					await RunFFMpeg();
+				}
+				catch { }
+				finally
+				{
+					lifetime.StopApplication();
+				}
+			});
+		});
+
+		return Task.CompletedTask;
+	}
+
+	public Task StopAsync(CancellationToken _)
+	{
+		Console.WriteLine("Stopping FFMpeg...");
+		cancelAction?.Invoke();
+		CleanupFiles();
+		return Task.CompletedTask;
+	}
+
+	private async Task RunFFMpeg()
+	{
+		string streamUri = Env.GetString("STREAM_URI", string.Empty);
+		string streamCache = Env.GetString("STREAM_CACHE", string.Empty);
+
+		var arguments = FFMpegArguments
+			.FromUrlInput(new Uri(streamUri))
+			.OutputToFile($"{streamCache}\\stream.m3u8", true, opts => opts
+				.WithVideoCodec("copy")
+				.WithAudioCodec("aac")
+				.UsingThreads(4)
+				.WithNoBanner()
+				.WithReconnect()
+				.WithReconnectDelay(2)
+				.ForceFormat("hls")
+				.WithHlsTime(4)
+				.WithHlsPlaylistSize(4)
+				.WithHlsSegmentFilename($"{streamCache}\\segment%04d.ts")
+				.WithHlsFlags(["delete_segments", "append_list", "split_by_time"])
+				.WithoutMetadata()
+				.WithFastStart()
+				.WithPersistentHttp()
+				.WithKeyframeInterval(8)
+				.WithFFlags(["nobuffer"])
+				.WithFlags(["low_delay"])
+				.StartAtZero());
+
+		Console.WriteLine($"Arguments: {arguments.Arguments}");
+
+		await arguments
+			.WithLogLevel(FFMpegLogLevel.Info)
+			.NotifyOnOutput(Console.WriteLine)
+			.NotifyOnError(Console.WriteLine)
+			.CancellableThrough(out cancelAction, 0)
+			.ProcessAsynchronously();
+	}
+
+	private static void CleanupFiles()
+	{
+		string streamCache = Env.GetString("STREAM_CACHE", string.Empty);
+		string[] files = Directory.GetFiles(streamCache, "*.ts");
+		files.ToList().ForEach(File.Delete);
+	}
+}

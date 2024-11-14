062e4dd9d658877ffbf2cd8003c26f577fa9b14b
svedev0 <emanuel@kulich.me>
2024-11-14 21:19:59 +0100

    Add request middleware

---
+++ Middleware/RequestMiddleware.cs
@@ -0,0 +1,33 @@
+﻿using System.Diagnostics;
+
+namespace RtspHlsRelay.Middleware;
+
+public class RequestMiddleware(RequestDelegate next)
+{
+	public async Task InvokeAsync(HttpContext context)
+	{
+		long startTime = Stopwatch.GetTimestamp();
+
+		Console.WriteLine("{0,-8}  {1,-6}  {2}{3}",
+			context.Request.Protocol,
+			context.Request.Method,
+			context.Request.Path,
+			context.Request.QueryString);
+
+		context.Response.OnStarting(() =>
+		{
+			context.Response.Headers.Server = "CustomServer";
+			context.Response.Headers.Accept = "application/json";
+			return Task.CompletedTask;
+		});
+
+		context.Response.OnCompleted(() =>
+		{
+			TimeSpan elapsed = Stopwatch.GetElapsedTime(startTime);
+			Console.WriteLine($"Request processed in {elapsed.TotalMilliseconds} ms");
+			return Task.CompletedTask;
+		});
+
+		await next(context);
+	}
+}
